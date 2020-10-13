d357337cfce45be6aa6cc36113de68c8dcb86add
SURE GOPINATH <suregopinath@gmail.com>
2020-10-13 18:17:50 +0530

    pincode validation for 6 numbers is done

--- pincodeValidation.sh
+++ pincodeValidation.sh
@@ -1,3 +1,18 @@
-#!/bin/bash
+#!/bin/bash -x
+
 echo "welcome to pincode validation"
+
+echo "Enter the pincode"
+read pincode
+
+pat="^[0-9]{6}$"
+
+if [[ $pincode =~ $pat ]]
+then
+        echo "valid"
+else
+        echo "not valid"
+fi
+
+
 
